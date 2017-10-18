e17ef2debcb1a57a12b875917af7612ea0e88a03
LordHieros <hieros_lord_invernalia@hotmail.com>
2017-10-18 14:14:25 +0200

    Trying new things is gooooood

---
+++ GUI/src/main/java/oesia/formacion/messenger/GUI/App.java
@@ -0,0 +1,13 @@
+package oesia.formacion.messenger.GUI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
