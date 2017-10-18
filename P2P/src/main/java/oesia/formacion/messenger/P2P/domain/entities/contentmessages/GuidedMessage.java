a4896f8476d11e1712671c410a46f51a428d8339
EXTmesegura <EXTmesegura@GO040086.itdeusto.local>
2017-10-18 16:30:40 +0200

    entidades bradcastMessage, guidedMessage y estados de los mensajes

--- P2P/src/main/java/oesia/formacion/messenger/P2P/domain/entities/contentmessages/GuidedMessage.java
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/domain/entities/contentmessages/GuidedMessage.java
@@ -6,15 +6,50 @@ import oesia.formacion.messenger.P2P.domain.entities.MessageType;
 
 public class GuidedMessage extends BroadcastMessage implements Serializable {
 
-	/**
-	 * 
-	 */
+	private String userDestinity;
 	private static final long serialVersionUID = -3251749199683153295L;
-	// TODO Code
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((userDestinity == null) ? 0 : userDestinity.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GuidedMessage other = (GuidedMessage) obj;
+		if (userDestinity == null) {
+			if (other.userDestinity != null)
+				return false;
+		} else if (!userDestinity.equals(other.userDestinity))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "GuidedMessage [userDestinity=" + userDestinity + "]";
+	}
+
+	public String getUserDestinity() {
+		return userDestinity;
+	}
+
+	public void setUserDestinity(String userDestinity) {
+		this.userDestinity = userDestinity;
+	}
 
 	@Override
 	public MessageType getType() {
 		return MessageType.GUIDED;
 	}
-	
+
 }
