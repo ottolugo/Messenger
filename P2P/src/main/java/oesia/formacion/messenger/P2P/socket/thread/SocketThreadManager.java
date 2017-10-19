e62a98ab6f43156b63fe7cbef39d59dac12cfefa
ramunoz <ramunoz@GO040089.itdeusto.local>
2017-10-19 12:04:56 +0200

    creado un thread managers

--- P2P/src/main/java/oesia/formacion/messenger/P2P/socket/thread/SocketThreadManager.java
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/socket/thread/SocketThreadManager.java
@@ -1,8 +1,11 @@
 package oesia.formacion.messenger.P2P.socket.thread;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Semaphore;
 
 public class SocketThreadManager {
@@ -15,22 +18,111 @@ public class SocketThreadManager {
 	}
 
 	/**
-	 * Cuando se crea un thread para mandar un mensage
+	 * Cuando se crea un thread para mandar un mensage nuevo lo agrega al listado
 	 * 
-	 * @param thread
+	 * @param thread que se lanzo para la insercion
 	 */
 	public void addSendMessageThread(Thread thread) {
 		checkinitialiceSendMessageThreads();
 		activeSendMessageThreads.add(thread);
 	}
 
+	/**
+	 * Se elimina un thread de la lista de envios por que acabo o no se necesita
+	 * 
+	 * @param thread a eliminar
+	 */
 	public void dropSendMessageThread(Thread thread) {
 		checkinitialiceSendMessageThreads();
 		activeSendMessageThreads.remove(thread);
 	}
 
+	/**
+	 * Se devuelve el listado de mensages activos
+	 * 
+	 * @return lista de thread activos para envio de mensages
+	 */
 	public List<Thread> getActiveSendMessageThreads() {
+		checkinitialiceSendMessageThreads();
+		Iterator<Thread> threadIterator = activeSendMessageThreads.iterator();
+		while (threadIterator.hasNext()) {
+			Thread active = threadIterator.next();
+			if (!active.isAlive()) {
+				dropSendMessageThread(active);
+			}
+		}
 		return activeSendMessageThreads;
+	}
+
+	/**
+	 * Se añade un nuevo hilo de lectura en un puerto.
+	 * 
+	 * @param port puerto que se lee
+	 * @param thread hilo que se ejecuta
+	 */
+	public void addPortListenerThread(int port, Thread thread) {
+		checkInicialiceListenerThread();
+		activePortListenerThreads.put(new Integer(port), thread);
+
+	}
+
+	/**
+	 * Fuerza parar un thread de escucha en un puerto
+	 * 
+	 * @param port puerto que se quiere dejar de escuchar
+	 */
+	public void stopListenerThread(int port) {
+		checkInicialiceListenerThread();
+		Thread tostop = null;
+		tostop = activePortListenerThreads.get(new Integer(port));
+		tostop.interrupt();
+		activePortListenerThreads.remove(new Integer(port));
+	}
+
+	/**
+	 * Se saca un puerto de la lista
+	 * 
+	 * @param port puerto que se deja
+	 */
+	public void dropPortListenerThread(int port) {
+		activePortListenerThreads.remove(new Integer(port));
+	}
+
+	/**
+	 * Se reciven los threads activos;
+	 * 
+	 * @return
+	 */
+	public Map<Integer, Thread> getPorListenerThreads() {
+		Set<Integer> keys = activePortListenerThreads.keySet();
+		keys.iterator();
+		for (Integer actualKey : keys) {
+			Thread thread = activePortListenerThreads.get(actualKey);
+			if (!thread.isAlive()) {
+				activePortListenerThreads.remove(actualKey, thread);
+			}
+		}
+
+		return activePortListenerThreads;
+	}
+
+	/**
+	 * para evitar que se manden nuevos mensages sin acabar antes de mandar uno.
+	 * 
+	 * @return el semaforo para los diferentes envios
+	 */
+	public Semaphore getSendMessageSemaphore() {
+		if (sendMessagePortSemaphore == null) {
+			sendMessagePortSemaphore = new Semaphore(1);
+		}
+		return sendMessagePortSemaphore;
+	}
+
+	private void checkInicialiceListenerThread() {
+		if (activePortListenerThreads == null) {
+			activePortListenerThreads = new HashMap<Integer, Thread>();
+		}
+
 	}
 
 	private void checkinitialiceSendMessageThreads() {
