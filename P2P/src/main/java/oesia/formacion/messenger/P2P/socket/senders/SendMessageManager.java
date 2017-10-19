2d4fb839dc3be2a97f9f86637bb2f9a3cff25051
ramunoz <ramunoz@GO040089.itdeusto.local>
2017-10-19 13:25:14 +0200

    Se implementa el lanzamiento del hilo de sendmesage

--- P2P/src/main/java/oesia/formacion/messenger/P2P/socket/senders/SendMessageManager.java
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/socket/senders/SendMessageManager.java
@@ -1,6 +1,7 @@
 package oesia.formacion.messenger.P2P.socket.senders;
 
 import oesia.formacion.messenger.P2P.domain.entities.Message;
+import oesia.formacion.messenger.P2P.socket.thread.SocketThreadManager;
 
 public class SendMessageManager {
 
@@ -15,7 +16,13 @@ public class SendMessageManager {
 	public static void sendMessage(Message msg) {
 
 		// Se prepara el thread, se lanza y se agrega a la lista de threads
+		Thread sendMessageThread = null;
+		SendMessageRunable runableToStart = null;
+		runableToStart = new SendMessageRunable(msg);
+		sendMessageThread = new Thread(runableToStart);
 
+		sendMessageThread.start();
+		SocketThreadManager.addSendMessageThread(sendMessageThread);
 	}
 
 }
