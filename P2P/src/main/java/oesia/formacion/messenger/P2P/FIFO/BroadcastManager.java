2964b71443d2962380a636c3d2bdff491668e4a8
jcagigas <jcagigas@GO040088.itdeusto.local>
2017-10-19 13:13:31 +0200

    p

---
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/FIFO/BroadcastManager.java
@@ -0,0 +1,18 @@
+package oesia.formacion.messenger.P2P.FIFO;
+
+import oesia.formacion.messenger.P2P.domain.entities.Message;
+
+public class BroadcastManager extends MessageManager {
+
+	public BroadcastManager(int limitTime, String user) {
+		super(limitTime, user);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void manageMessage(Message message) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- P2P/src/main/java/oesia/formacion/messenger/P2P/FIFO/FifoStack.java
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/FIFO/FifoStack.java
@@ -1,32 +1,57 @@
-package oesia.formacion.messenger.P2P.domain.FIFO;
+package oesia.formacion.messenger.P2P.FIFO;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import oesia.formacion.messenger.P2P.domain.entities.Message;
 
-public class FifoStack {
-	private List<Message> messageList;
+public class FifoStack extends Thread {
+	private List<Message> fifoStack;
+	private int limitTime;
+	private String user;
 
-	/**
-	 * 
-	 */
-	public FifoStack() {
-		this.messageList = new ArrayList<Message>();
+	public FifoStack(int limitTime, String user) {
+		fifoStack = new ArrayList<Message>();
+		this.limitTime = limitTime;
+		this.user = user;
 	}
 
 	public void addMessage(Message message) {
-		messageList.add(message);
-	}
-
-	public void removeMessage(Message message) {
-		messageList.remove(message);
+		fifoStack.add(message);
 	}
 
 	public Message getMessage() {
-		Message message = messageList.get(0);
-		removeMessage(messageList.get(0));
+		Message message = fifoStack.get(0);
+		fifoStack.remove(0);
 		return message;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			if (fifoStack.size() > 0) {
+				Message message = getMessage();
+				MessageManager messageManager;
+				switch (message.getType()) {
+				case ACK:
+					messageManager = MessageFactory.getAckManager(limitTime, user);
+					break;
+				case BROADCAST:
+					messageManager = MessageFactory.getBroacastManager(limitTime, user);
+					break;
+				case GUIDED:
+					messageManager = MessageFactory.getGuidedmanager(limitTime, user);
+					break;
+				case KEEPALIVE:
+					messageManager = MessageFactory.getKeepAliveManager(limitTime, user);
+					break;
+				default:
+					messageManager = null;
+					break;
+				}
+				messageManager.manageMessage(message);
+			}
+		}
 	}
 
 }
