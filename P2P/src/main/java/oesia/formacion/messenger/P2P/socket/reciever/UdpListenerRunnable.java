b37b83e8575a9d94d07d1f852eb3a67c04d58d73
ramunoz <ramunoz@GO040089.itdeusto.local>
2017-10-19 10:52:03 +0200

    A las 11 desayuno

--- P2P/src/main/java/oesia/formacion/messenger/P2P/socket/reciever/UdpListenerRunnable.java
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/socket/reciever/UdpListenerRunnable.java
@@ -10,6 +10,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import oesia.formacion.messenger.P2P.domain.entities.Message;
+import oesia.formacion.messenger.P2P.domain.managers.SocketMessageManager;
 import oesia.formacion.messenger.P2P.socket.configuration.SocketConfiguration;
 
 public class UdpListenerRunnable implements Runnable {
@@ -30,8 +31,10 @@ public class UdpListenerRunnable implements Runnable {
 	public void run() {
 		DatagramSocket socket = null;
 		try {
+			// Se saca primero el tamaño del datagrama de la configuracion del socket
+			int bytes = SocketConfiguration.DATAGRAMSIZE;
 			socket = new DatagramSocket(socketPort);
-			byte[] incomingData = new byte[SocketConfiguration.DATAGRAMSIZE];
+			byte[] incomingData = new byte[bytes];
 			while (true) {
 				DatagramPacket datagramPacket = new DatagramPacket(incomingData, socketPort);
 				// Se pide al socket que este a la escucha
@@ -47,9 +50,10 @@ public class UdpListenerRunnable implements Runnable {
 				try {
 					Message message = (Message) objectInputStream.readObject();
 					LOG.log(Level.INFO, "Se recepciono el mensage con codigo: " + message.getCode().toString());
-					// FIXME Necesito un manager al que entregar el mensage
+					SocketMessageManager.receiveMessage(message);
 
 				} catch (ClassNotFoundException e) {
+					// Este Catch es por si se recive algo que no sea un objeto
 					Object recivido = new Object();
 					try {
 						recivido = objectInputStream.readObject();
