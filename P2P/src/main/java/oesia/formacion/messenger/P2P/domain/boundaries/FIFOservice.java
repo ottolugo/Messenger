cc2597dbd2181f3bced8b735c0eaccfe04cdc479
jcagigas <jcagigas@GO040088.itdeusto.local>
2017-10-19 09:58:38 +0200

    fifoput a fifoservice

--- P2P/src/main/java/oesia/formacion/messenger/P2P/domain/boundaries/FIFOservice.java
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/domain/boundaries/FIFOservice.java
@@ -2,6 +2,10 @@ package oesia.formacion.messenger.P2P.domain.boundaries;
 
 import oesia.formacion.messenger.P2P.domain.entities.Message;
 
-public interface FIFOput {
+public interface FIFOservice {
+	/**
+	 * Put message in fifoStack
+	 * @param msg
+	 */
 	public void put(Message msg);
 }
