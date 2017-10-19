a7e76678f6862c932fa171483ced15f12c279978
LordHieros <hieros_lord_invernalia@hotmail.com>
2017-10-19 10:15:38 +0200

    Limpieza y cosas varias

--- P2P/src/main/java/oesia/formacion/messenger/P2P/domain/boundaries/externals/MessageObserverManager.java
+++
@@ -1,7 +0,0 @@
-package oesia.formacion.messenger.P2P.domain.boundaries.externals;
-
-public interface MessageObserverManager {
-	public void addObserver(MessageObserver newObserver);
-	public void dropObserver(MessageObserver oldObserver);
-
-}
