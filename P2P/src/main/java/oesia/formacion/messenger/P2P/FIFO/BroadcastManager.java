c5cf7e2d071a9757b9990b892b5e5b7424892a2c
jcagigas <jcagigas@GO040088.itdeusto.local>
2017-10-19 13:47:30 +0200

    chi-chi-chi-chi-changes!!

--- P2P/src/main/java/oesia/formacion/messenger/P2P/FIFO/BroadcastManager.java
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/FIFO/BroadcastManager.java
@@ -1,17 +1,20 @@
 package oesia.formacion.messenger.P2P.FIFO;
 
 import oesia.formacion.messenger.P2P.domain.entities.Message;
+import oesia.formacion.messenger.P2P.domain.entities.contentmessages.BroadcastMessage;
+import oesia.formacion.messenger.P2P.domain.managers.FIFOMessageManager;
 
 public class BroadcastManager extends MessageManager {
 
 	public BroadcastManager(int limitTime, String user) {
 		super(limitTime, user);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void manageMessage(Message message) {
-		// TODO Auto-generated method stub
+		if (this.isDateValid(message)) {
+			FIFOMessageManager.receiveBroadcast((BroadcastMessage) message);
+		}
 
 	}
 
--- P2P/src/main/java/oesia/formacion/messenger/P2P/FIFO/MessageManager.java
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/FIFO/MessageManager.java
@@ -1,5 +1,8 @@
 package oesia.formacion.messenger.P2P.FIFO;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 import oesia.formacion.messenger.P2P.domain.entities.Message;
 
 public abstract class MessageManager {
@@ -20,6 +23,25 @@ public abstract class MessageManager {
 
 	public String getUser() {
 		return user;
+	}
+
+	public boolean isValid(Message message) {
+		LocalDateTime date = LocalDateTime.now();
+		if (message.getCode().getDate().until(date, ChronoUnit.SECONDS) < limitTime
+				&& message.getCode().getUser().equals(user)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean isDateValid(Message message) {
+		LocalDateTime date = LocalDateTime.now();
+		if (message.getCode().getDate().until(date, ChronoUnit.SECONDS) < limitTime) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
