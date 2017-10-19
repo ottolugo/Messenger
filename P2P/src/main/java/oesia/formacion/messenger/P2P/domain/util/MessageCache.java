60a30cbc9d7dd83ba59b678d2519ccef49e5ac32
ramunoz <ramunoz@GO040089.itdeusto.local>
2017-10-19 14:40:51 +0200

    revision

---
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/domain/util/MessageCache.java
@@ -0,0 +1,26 @@
+package oesia.formacion.messenger.P2P.domain.util;
+
+import java.util.HashMap;
+
+import oesia.formacion.messenger.P2P.domain.entities.Code;
+import oesia.formacion.messenger.P2P.domain.entities.contentmessages.BroadcastMessage;
+import oesia.formacion.messenger.P2P.domain.entities.contentmessages.MessageStatus;
+
+public class MessageCache {
+	private HashMap<Code, BroadcastMessage> messages;
+
+	MessageCache() {
+		messages = new HashMap<Code, BroadcastMessage>();
+	}
+
+	public void addMessage(BroadcastMessage msg) {
+		messages.put(msg.getCode(), msg);
+	}
+
+	public void updateMessage(Code code, MessageStatus status) {
+		BroadcastMessage msg = messages.get(code);
+		if (msg != null) {
+			msg.setStatus(status);
+		}
+	}
+}
---
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/repository/RepositoryFactory.java
@@ -0,0 +1,19 @@
+package oesia.formacion.messenger.P2P.repository;
+
+import oesia.formacion.messenger.P2P.domain.boundaries.RepositoryService;
+
+public class RepositoryFactory {
+
+	private static RepositoryService repositoryService = null;
+
+	public RepositoryFactory() {
+	}
+
+	public static RepositoryService getRepositoryService() {
+		if (repositoryService == null) {
+			repositoryService = new RepositoryServiceImpl();
+		}
+
+		return repositoryService;
+	}
+}
