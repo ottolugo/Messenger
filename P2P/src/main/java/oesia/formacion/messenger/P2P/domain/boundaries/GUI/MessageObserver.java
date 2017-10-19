bbc39f825676a357c578563eea8655b0b5e88cee
LordHieros <hieros_lord_invernalia@hotmail.com>
2017-10-19 12:47:50 +0200

    Interfaces GUI

---
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/domain/boundaries/GUI/MessageObserver.java
@@ -0,0 +1,12 @@
+package oesia.formacion.messenger.P2P.domain.boundaries.GUI;
+
+import oesia.formacion.messenger.P2P.domain.entities.contentmessages.MessageStatus;
+
+public interface MessageObserver {
+	/**
+	 * Executed when the messages status is changed
+	 * 
+	 * @param status
+	 */
+	public void notify(MessageStatus status);
+}
