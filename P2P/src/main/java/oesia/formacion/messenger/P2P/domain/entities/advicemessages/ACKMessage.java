2c496788f2a8eba9d99361cc4e2c8ee223acda4f
LordHieros <hieros_lord_invernalia@hotmail.com>
2017-10-20 08:22:54 +0200

    Code respost renombrado a code response

--- P2P/src/main/java/oesia/formacion/messenger/P2P/domain/entities/advicemessages/ACKMessage.java
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/domain/entities/advicemessages/ACKMessage.java
@@ -13,19 +13,19 @@ public class ACKMessage extends Message implements Serializable {
 	 */
 	private static final long serialVersionUID = 132496653241888073L;
 
-	private Code codeRespost;
+	private Code codeResponse;
 
 	public ACKMessage(Code code, Code codeRespost) {
 		super(code);
-		this.codeRespost = codeRespost;
+		this.codeResponse = codeRespost;
 	}
 
 	public Code getCodeRespost() {
-		return codeRespost;
+		return codeResponse;
 	}
 
 	public void setCodeRespost(Code codeRespost) {
-		this.codeRespost = codeRespost;
+		this.codeResponse = codeRespost;
 	}
 
 	@Override
@@ -35,14 +35,14 @@ public class ACKMessage extends Message implements Serializable {
 
 	@Override
 	public String toString() {
-		return "ACKMessage [codeRespost=" + codeRespost + "]";
+		return "ACKMessage [codeRespost=" + codeResponse + "]";
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
-		result = prime * result + ((codeRespost == null) ? 0 : codeRespost.hashCode());
+		result = prime * result + ((codeResponse == null) ? 0 : codeResponse.hashCode());
 		return result;
 	}
 
@@ -55,10 +55,10 @@ public class ACKMessage extends Message implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		ACKMessage other = (ACKMessage) obj;
-		if (codeRespost == null) {
-			if (other.codeRespost != null)
+		if (codeResponse == null) {
+			if (other.codeResponse != null)
 				return false;
-		} else if (!codeRespost.equals(other.codeRespost))
+		} else if (!codeResponse.equals(other.codeResponse))
 			return false;
 		return true;
 	}
