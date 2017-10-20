4ac1efd6ae3085ba5df26c1a01e9ca7de926bb5d
LordHieros <hieros_lord_invernalia@hotmail.com>
2017-10-20 11:30:37 +0200

    Cosas Marientas explotantes recalibradas

---
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/domain/configuration/KeepAliverConfiguration.java
@@ -0,0 +1,15 @@
+package oesia.formacion.messenger.P2P.domain.configuration;
+
+import oesia.formacion.messenger.P2P.domain.boundaries.KeepAliverService;
+import oesia.formacion.messenger.P2P.keepAliver.KeepAliverServiceImpl;
+
+public class KeepAliverConfiguration {
+private static KeepAliverService service;
+	
+	public static KeepAliverService getKeepAliverService(){
+		if(service == null){
+			service = new KeepAliverServiceImpl();
+		}
+		return service;
+	}
+}
