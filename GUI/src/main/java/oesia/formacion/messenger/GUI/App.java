22c0a92f4bf9505d366191c43ab21a5013bcb67a
LordHieros <hieros_lord_invernalia@hotmail.com>
2017-10-23 14:20:19 +0200

    Cosas algo mejoradas pero aun altamente explosivas, if you catch my meaning...

--- GUI/src/main/java/oesia/formacion/messenger/GUI/App.java
+++ GUI/src/main/java/oesia/formacion/messenger/GUI/App.java
@@ -1,5 +1,6 @@
 package oesia.formacion.messenger.GUI;
 
+import java.io.IOException;
 import java.util.List;
 
 import javafx.application.Application;
@@ -15,52 +16,49 @@ import oesia.formacion.messenger.GUI.entities.MessageGui;
  */
 public class App extends Application {
 
-    private static ChatController cc = new ChatController();
-    private static App app;
+	private static ChatController cc;
+	private static App app;
 
-    public static App getApp() {
-	return app;
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-	app = this;
-	Parent root = FXMLLoader.load(getClass().getResource("FXMLChat.fxml"));
-
-	Scene scene = new Scene(root);
-	// scene.getStylesheets().add(getClass().getResource("estilo.css").toExternalForm());
-	stage.setScene(scene);
-	stage.setTitle("ITChat");
-	// stage.getIcons()
-	stage.show();
-
-    }
-
-    public void setUserList(List<String> userList) {
-	if (cc == null) {
-	    cc = new ChatController();
+	public static App getApp() {
+		return app;
 	}
-	cc.setUserList(userList);
 
-    }
+	@Override
+	public void start(Stage stage) throws Exception {
+		app = this;
+		Parent root = null;
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("FXMLChat.fxml"));
+			root = loader.load();
+			cc = loader.getController();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Scene scene = new Scene(root);
+		// scene.getStylesheets().add(getClass().getResource("estilo.css").toExternalForm());
+		stage.setScene(scene);
+		stage.setTitle("ITChat");
+		// stage.getIcons()
+		stage.show();
 
-    public void addMessage(MessageGui message) {
-	if (cc == null) {
-	    cc = new ChatController();
 	}
-	cc.addMessage(message);
-    }
 
-    public static void main(String[] args) {
-	Launch.start();
-	launch(args);
-	// System.out.println("TRP!!!!");
+	public void setUserList(List<String> userList) {
+		cc.setUserList(userList);
+	}
 
-	System.exit(0);
+	public void addMessage(MessageGui message) {
+		cc.addMessage(message);
+	}
 
-    }
+	public static void main(String[] args) {
+		Launch.start();
+		launch(args);
+		System.exit(0);
+	}
 
-    public ChatController getChatController() {
-	return cc;
-    }
+	public ChatController getChatController() {
+		return cc;
+	}
 }
--- GUI/src/main/java/oesia/formacion/messenger/GUI/ChatController.java
+++ GUI/src/main/java/oesia/formacion/messenger/GUI/ChatController.java
@@ -18,104 +18,104 @@ import oesia.formacion.messenger.GUI.boundaries.MessageManagerFactory;
 import oesia.formacion.messenger.GUI.entities.MessageGui;
 
 public class ChatController implements Initializable {
-    @FXML
-    ListView<MessageGui> lvMensajes;
-    @FXML
-    TextArea taMensaje;
-    @FXML
-    ListView<String> lvUser;
-    int cosas = 0;
+	@FXML
+	ListView<MessageGui> lvMensajes;
+	@FXML
+	TextArea taMensaje;
+	@FXML
+	ListView<String> lvUser;
+	int cosas = 0;
 
-    private static ObservableList<MessageGui> mensagges = FXCollections.observableArrayList();
-    private static ObservableList<String> items = FXCollections.observableArrayList();
+	private ObservableList<MessageGui> mensagges = FXCollections.observableArrayList();
+	private ObservableList<String> items = FXCollections.observableArrayList();
 
-    public void initialize(URL arg0, ResourceBundle arg1) {
+	public void initialize(URL arg0, ResourceBundle arg1) {
 
-	lvMensajes.setItems(mensagges);
-	lvUser.setItems(items);
+		lvMensajes.setItems(mensagges);
+		lvUser.setItems(items);
 
-	lvMensajes.setCellFactory(new Callback<ListView<MessageGui>, ListCell<MessageGui>>() {
+		lvMensajes.setCellFactory(new Callback<ListView<MessageGui>, ListCell<MessageGui>>() {
 
-	    public ListCell<MessageGui> call(ListView<MessageGui> param) {
-		ListCell<MessageGui> cell = new ListCell<MessageGui>() {
+			public ListCell<MessageGui> call(ListView<MessageGui> param) {
+				ListCell<MessageGui> cell = new ListCell<MessageGui>() {
 
-		    @Override
-		    protected void updateItem(MessageGui item, boolean empty) {
-			super.updateItem(item, empty);
-			if (item != null) {
-			    setText(item.toString());
+					@Override
+					protected void updateItem(MessageGui item, boolean empty) {
+						super.updateItem(item, empty);
+						if (item != null) {
+							setText(item.toString());
 
-			    MessageManager mm = MessageManagerFactory.getMessageManager();
-			    if (item.getSender().equals(mm.whoIAm())) {
+							MessageManager mm = MessageManagerFactory.getMessageManager();
+							if (item.getSender().equals(mm.whoIAm())) {
 
-				switch (item.getStatus()) {
-				    case ARRIVED:
-					setStyle("-fx-background-color:#ABEBC6");
-					setAlignment(Pos.BASELINE_RIGHT);
-					break;
-				    case CANCELED:
-					setStyle("-fx-background-color:#F98282;");
-					setAlignment(Pos.BASELINE_RIGHT);
-					break;
-				    case NEW:
-					setStyle("-fx-background-color:#F0B47D;");
-					setAlignment(Pos.BASELINE_RIGHT);
-					break;
-				    case SENT:
-					setStyle("-fx-background-color:#F0EA7D");
-					setAlignment(Pos.BASELINE_RIGHT);
-					break;
-				}
-			    } else {
+								switch (item.getStatus()) {
+								case ARRIVED:
+									setStyle("-fx-background-color:#ABEBC6");
+									setAlignment(Pos.BASELINE_RIGHT);
+									break;
+								case CANCELED:
+									setStyle("-fx-background-color:#F98282;");
+									setAlignment(Pos.BASELINE_RIGHT);
+									break;
+								case NEW:
+									setStyle("-fx-background-color:#F0B47D;");
+									setAlignment(Pos.BASELINE_RIGHT);
+									break;
+								case SENT:
+									setStyle("-fx-background-color:#F0EA7D");
+									setAlignment(Pos.BASELINE_RIGHT);
+									break;
+								}
+							} else {
 
-				setWrapText(true);
-				setAlignment(Pos.BASELINE_LEFT);
-				setStyle(
-					"-fx-border-color: #ABEBC6;-fx-border-insets: 2;-fx-border-width: 2;-fx-border-style: solid;-fx-border-radius: 25px;-fx-background-color:#ABD9EB;");
-			    }
+								setWrapText(true);
+								setAlignment(Pos.BASELINE_LEFT);
+								setStyle(
+										"-fx-border-color: #ABEBC6;-fx-border-insets: 2;-fx-border-width: 2;-fx-border-style: solid;-fx-border-radius: 25px;-fx-background-color:#ABD9EB;");
+							}
+
+						}
+
+					}
+				};
+				return cell;
+			}
+		});
+
+	}
+
+	@FXML
+	public void sendMensaje() {
+		MessageManager messageManager = MessageManagerFactory.getMessageManager();
+		if (!taMensaje.getText().equals("")) {
+			int indice = lvUser.getSelectionModel().getSelectedIndex();
+			MessageGui mensaje;
+			if (indice >= 0) {
+				String nombre = lvUser.getSelectionModel().getSelectedItem();
+				mensaje = new MessageGui(taMensaje.getText(), messageManager.whoIAm(), nombre);
+			} else {
+
+				mensaje = new MessageGui(taMensaje.getText(), messageManager.whoIAm());
 
 			}
-
-		    }
-		};
-		return cell;
-	    }
-	});
-
-    }
-
-    @FXML
-    public void sendMensaje() {
-	MessageManager messageManager = MessageManagerFactory.getMessageManager();
-	if (!taMensaje.getText().equals("")) {
-	    int indice = lvUser.getSelectionModel().getSelectedIndex();
-	    MessageGui mensaje;
-	    if (indice >= 0) {
-		String nombre = lvUser.getSelectionModel().getSelectedItem();
-		mensaje = new MessageGui(taMensaje.getText(), messageManager.whoIAm(), nombre);
-	    } else {
-
-		mensaje = new MessageGui(taMensaje.getText(), messageManager.whoIAm());
-
-	    }
-	    mensagges.add(mensaje);
-	    taMensaje.setText("");
-	    messageManager.sendMessage(mensaje);
+			mensagges.add(mensaje);
+			taMensaje.setText("");
+			messageManager.sendMessage(mensaje);
+		}
 	}
-    }
 
-    @FXML
-    public void desselectUser() {
-	lvUser.getSelectionModel().clearSelection();
-    }
+	@FXML
+	public void desselectUser() {
+		lvUser.getSelectionModel().clearSelection();
+	}
 
-    public void setUserList(List<String> userlist) {
-	items.clear();
-	items.addAll(userlist);
-    }
+	public void setUserList(List<String> userlist) {
+		items.clear();
+		items.addAll(userlist);
+	}
 
-    public void addMessage(MessageGui message) {
-	mensagges.add(message);
-    }
+	public void addMessage(MessageGui message) {
+		mensagges.add(message);
+	}
 
 }
