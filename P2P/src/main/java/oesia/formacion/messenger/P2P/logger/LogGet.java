37759da2bbe94e55d05c6140da457f3619e7b17d
ramunoz <ramunoz@GO040089.mshome.net>
2017-10-23 08:51:32 +0200

    Se mejora el sistema de logs. Ahora crea un ../Logger.log

--- P2P/src/main/java/oesia/formacion/messenger/P2P/logger/LogGet.java
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/logger/LogGet.java
@@ -1,7 +1,11 @@
 package oesia.formacion.messenger.P2P.logger;
 
+import java.io.IOException;
 import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.XMLFormatter;
 
 public class LogGet {
 
@@ -11,8 +15,16 @@ public class LogGet {
 	public static Logger getLogger(Class<?> callerClass) {
 		Logger logConfigurated = Logger.getLogger(callerClass.getName());
 		ConsoleHandler handler = new ConsoleHandler();
+		handler.setLevel(Level.WARNING);
 		handler.setFormatter(new ConsoleLogFormatter());
 		logConfigurated.addHandler(handler);
+		try {
+			FileHandler saveHandler = new FileHandler("../Logger.log", true);
+			saveHandler.setFormatter(new XMLFormatter());
+			saveHandler.setLevel(Level.ALL);
+		} catch (SecurityException e) {
+		} catch (IOException e) {
+		}
 		return logConfigurated;
 	}
 }
