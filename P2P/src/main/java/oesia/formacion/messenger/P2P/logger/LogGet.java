582011611d03df586c994caaa60ffa673b394b15
ramunoz <ramunoz@GO040089.mshome.net>
2017-10-23 09:12:24 +0200

    Se mejora el sistema de logs.

--- P2P/src/main/java/oesia/formacion/messenger/P2P/logger/LogGet.java
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/logger/LogGet.java
@@ -1,18 +1,43 @@
 package oesia.formacion.messenger.P2P.logger;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.XMLFormatter;
 
+/**
+ * Clase estatica que te da un log
+ * 
+ * @author ramunoz
+ *
+ */
 public class LogGet {
+
+	private static Map<Class<?>, Logger> logMap = null;
 
 	private LogGet() {
 	}
 
+	/**
+	 * Crea el loger aplicandole los diferentes manejadores para poder usarse
+	 * 
+	 * @param callerClass clase que llama al logger
+	 * @return un loger configurado
+	 */
 	public static Logger getLogger(Class<?> callerClass) {
+		Logger log = null;
+		if (logMap == null) {
+			logMap = new HashMap<Class<?>, Logger>();
+		}
+		log = logMap.containsKey(callerClass) ? logMap.get(callerClass) : createLog(callerClass);
+		return log;
+	}
+
+	private static Logger createLog(Class<?> callerClass) {
 		Logger logConfigurated = Logger.getLogger(callerClass.getName());
 		ConsoleHandler handler = new ConsoleHandler();
 		// El nivel para los que se muestran por pantalla
@@ -27,6 +52,7 @@ public class LogGet {
 		} catch (SecurityException e) {
 		} catch (IOException e) {
 		}
+		logMap.put(callerClass, logConfigurated);
 		return logConfigurated;
 	}
 }
