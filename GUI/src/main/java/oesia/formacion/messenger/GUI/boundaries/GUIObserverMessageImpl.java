5529b036aef3dd1ec7f6c1a3f60164b706d6cabb
Otto <30315035+ottolugo@users.noreply.github.com>
2017-10-23 13:57:55 +0200

    Cosas nazis que son buenas

--- GUI/src/main/java/oesia/formacion/messenger/GUI/boundaries/GUIObserverMessageImpl.java
+++ GUI/src/main/java/oesia/formacion/messenger/GUI/boundaries/GUIObserverMessageImpl.java
@@ -1,16 +1,17 @@
 package oesia.formacion.messenger.GUI.boundaries;
 
-import oesia.formacion.messenger.GUI.ChatController;
+import oesia.formacion.messenger.GUI.App;
 import oesia.formacion.messenger.GUI.util.Converter;
 import oesia.formacion.messenger.P2P.domain.boundaries.GUI.GUIObserver;
 import oesia.formacion.messenger.P2P.domain.entities.contentmessages.UserMessage;
 
 public class GUIObserverMessageImpl implements GUIObserver<UserMessage> {
 
-	public void update(UserMessage obj) {
-		// TODO Auto-generated method stub
-		ChatController.mensagges.add(Converter.convertIntoMessage(obj));
-		System.out.println("Mensaje recibido:" + obj);
-	}
+    public void update(UserMessage obj) {
+	// TODO Auto-generated method stub
+	// ChatController.mensagges.add(Converter.convertIntoMessage(obj));
+	App.getApp().addMessage(Converter.convertIntoMessage(obj));
+	System.out.println("Mensaje recibido:" + obj);
+    }
 
 }
