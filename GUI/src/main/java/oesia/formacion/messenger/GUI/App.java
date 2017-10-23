f3d9f0b7cf836f40d4a121e5219c7f7e5e9e1299
Otto <30315035+ottolugo@users.noreply.github.com>
2017-10-23 13:20:18 +0200

    Implementado para que cierre

--- GUI/src/main/java/oesia/formacion/messenger/GUI/App.java
+++ GUI/src/main/java/oesia/formacion/messenger/GUI/App.java
@@ -11,21 +11,24 @@ import javafx.stage.Stage;
  *
  */
 public class App extends Application {
-	@Override
-	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("FXMLChat.fxml"));
+    @Override
+    public void start(Stage stage) throws Exception {
+	Parent root = FXMLLoader.load(getClass().getResource("FXMLChat.fxml"));
 
-		Scene scene = new Scene(root);
-		// scene.getStylesheets().add(getClass().getResource("estilo.css").toExternalForm());
-		stage.setScene(scene);
-		stage.setTitle("ITChat");
-		// stage.getIcons()
-		stage.show();
-	}
+	Scene scene = new Scene(root);
+	// scene.getStylesheets().add(getClass().getResource("estilo.css").toExternalForm());
+	stage.setScene(scene);
+	stage.setTitle("ITChat");
+	// stage.getIcons()
+	stage.show();
+    }
 
-	public static void main(String[] args) {
-		Launch.start();
-		launch(args);
-		// System.out.println("TRP!!!!");
-	}
+    public static void main(String[] args) {
+	Launch.start();
+	launch(args);
+	// System.out.println("TRP!!!!");
+
+	System.exit(0);
+
+    }
 }
