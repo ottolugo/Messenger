579bfc3a954f23e1e77582994de3a72a3d8da821
yvnpaz <yppousa@esei.uvigo.es>
2017-10-25 08:20:33 +0200

    XMl nuevo

--- P2P/src/main/java/oesia/formacion/messenger/P2P/repository/boundaries/RepositoryServiceImpl.java
+++ P2P/src/main/java/oesia/formacion/messenger/P2P/repository/boundaries/RepositoryServiceImpl.java
@@ -1,45 +1,42 @@
 package oesia.formacion.messenger.P2P.repository.boundaries;
 
-import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
-
 import oesia.formacion.messenger.P2P.domain.boundaries.RepositoryService;
 import oesia.formacion.messenger.P2P.domain.entities.LocalConfiguration;
 import oesia.formacion.messenger.P2P.domain.entities.contentmessages.UserMessage;
 import oesia.formacion.messenger.P2P.repository.RepositoryFactory;
 import oesia.formacion.messenger.P2P.repository.configuration.DataConfiguration;
-import oesia.formacion.messenger.P2P.repository.manager.ManagerRepositoryServiceImpl;
+import oesia.formacion.messenger.P2P.repository.configuration.logMessage.InLog;
+import oesia.formacion.messenger.P2P.repository.configuration.xmlMessage.XmlMessage;
 
 public class RepositoryServiceImpl implements RepositoryService {
 
-	private final Logger LOGMSG = Logger.getLogger(ManagerRepositoryServiceImpl.class.getName());
-	private final ManagerRepositoryService managerRepositoryService;
+//	private final static Logger LOGMSG = Logger.getLogger( RepositoryServiceImpl.class.getName() );
 	private final DataConfiguration dataConfiguration;
+	private final InLog inLog = new InLog();
+	private final XmlMessage xmlMessage;
 
 	public RepositoryServiceImpl() {
-		FileHandler logFileHandler;
-		try {
-			logFileHandler = new FileHandler("../msg.log", true);
-			logFileHandler.setFormatter(new SimpleFormatter());
-			LOGMSG.addHandler(logFileHandler);
-		} catch (SecurityException | IOException e1) {
-		}
+//		FileHandler logFileHandler;
+//		try {
+//			logFileHandler = new FileHandler("../msg.log", true);
+//			logFileHandler.setFormatter(new SimpleFormatter());
+//			LOGMSG.addHandler(logFileHandler);
+//		} catch (SecurityException | IOException e1) {
+//		}
+		xmlMessage = new XmlMessage();
+		inLog.setupLogger();
 		dataConfiguration = RepositoryFactory.getDataConfiguration();
 		
-		this.managerRepositoryService = RepositoryFactory.getManagerRepositoryService();
 	}
 
 	@Override
 	public void logMessage(UserMessage msg) {
-		LOGMSG.info(msg.toString());
-//		this.managerRepositoryService.insertLog(msg);
+//		LOGMSG.info(msg.toString());
+		inLog.test(msg);
 	}
 
 	@Override
 	public LocalConfiguration getConfiguration() {
-//		LocalConfiguration reciveLocalConfig = this.managerRepositoryService.loadXml();
 		LocalConfiguration reciveLocalConfig = new LocalConfiguration(dataConfiguration.getWhoIam(),
 				dataConfiguration.getKalTime(), 
 				dataConfiguration.getAckTimeout(),
