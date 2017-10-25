56914816cea0cc5e6c1efecc88aeedee53ee1301
LordHieros <hieros_lord_invernalia@hotmail.com>
2017-10-25 11:22:11 +0200

    flores varias y alertas emergentes

--- GUI/src/main/java/oesia/formacion/messenger/GUI/Checker.java
+++ GUI/src/main/java/oesia/formacion/messenger/GUI/Checker.java
@@ -5,15 +5,13 @@ import oesia.formacion.messenger.GUI.entities.MessageGui;
 
 public class Checker {
 	public static void check(MessageGui item) {
-		if (item.getSender().equals("EXTamarino") || item.getSender().equals("EXTmesegura")) {
-			if (item.getMessage().matches("\\[GOD\\] .*")) {
-				item.setMessage(item.getMessage().split("\\] ")[1]);
-				Alert alert = new Alert(Alert.AlertType.WARNING);
-				alert.setTitle("DIOS HA HABLADO!!!");
-				alert.setHeaderText(null);
-				alert.setContentText("Y ha dicho " + item.getMessage());
-				alert.show();
-			}
+		if (item.getMessage().matches("\\[ALERT\\] .*")) {
+			item.setMessage(item.getMessage().split("\\] ")[1]);
+			Alert alert = new Alert(Alert.AlertType.WARNING);
+			alert.setTitle("Mensaje Emergente!!!");
+			alert.setHeaderText(null);
+			alert.setContentText(item.getMessage());
+			alert.show();
 		}
 	}
 }
