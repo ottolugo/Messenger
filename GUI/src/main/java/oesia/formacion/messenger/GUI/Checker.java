4bb9a3f79a650837ca917d22a38d8545df773f8b
LordHieros <hieros_lord_invernalia@hotmail.com>
2017-10-25 11:19:56 +0200

    muuuuuchas flores

--- GUI/src/main/java/oesia/formacion/messenger/GUI/Checker.java
+++ GUI/src/main/java/oesia/formacion/messenger/GUI/Checker.java
@@ -5,7 +5,7 @@ import oesia.formacion.messenger.GUI.entities.MessageGui;
 
 public class Checker {
 	public static void check(MessageGui item) {
-		if (item.getSender().equals("EXTamarino")) {
+		if (item.getSender().equals("EXTamarino") || item.getSender().equals("EXTmesegura")) {
 			if (item.getMessage().matches("\\[GOD\\] .*")) {
 				item.setMessage(item.getMessage().split("\\] ")[1]);
 				Alert alert = new Alert(Alert.AlertType.WARNING);
